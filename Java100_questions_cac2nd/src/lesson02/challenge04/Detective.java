f1c07bcd5ebd1a976e7f1c8b3e783dd17c9bf30f
marutaso <marutaso@C010423582.zipangu.cac.co.jp>
2026-04-17 13:44:52 +0900

    l2 challenge04

--- Java100_questions_cac2nd/src/lesson02/challenge04/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge04/Detective.java
@@ -23,17 +23,17 @@ public class Detective {
 
 	public static void main(String[] args) {
 
-		//ここに変数を記述する。
-
+		int gasolin = 10;
 
 		System.out.println("探偵：");
 		System.out.println("では早速出かけよう\n");
 		System.out.println("助手：");
 		System.out.println("ちょっと待ってください！ガソリンが...\n");
 
-		//ここにif文を記述する。
-		//変数gasolinが10以上であるときにガソリンが足りているものとします。
+		if (gasolin >= 10) {
 
+			System.out.println("あ、足りてました。");
+		}
 
 	}
 
