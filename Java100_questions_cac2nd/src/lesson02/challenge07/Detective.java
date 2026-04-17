423a90ab2abf2f45b3b23a0773a56904cc0bfdc4
marutaso <marutaso@C010423582.zipangu.cac.co.jp>
2026-04-17 16:32:57 +0900

    l2 challenge07

--- Java100_questions_cac2nd/src/lesson02/challenge07/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge07/Detective.java
@@ -25,7 +25,9 @@
 
 package lesson02.challenge07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Detective {
 
@@ -36,9 +38,17 @@ public class Detective {
 		System.out.println("あなたが思う犯人の身長を適当に入力してみてください。\n");
 		System.out.print("ここに入力＞");
 
-		//ここに入力処理を入れる
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int height = Integer.parseInt(str);
 
-		//ここにif～else文を入れる
+		if (height >= 185) {
+			System.out.println(" その身長だと犯行が可能ですね。");
+
+		} else {
+			System.out.println(" その身長だと犯行は無理ですね。");
+
+		}
 
 	}
 
